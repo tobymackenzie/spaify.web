933801a8eb965b8c9c1dbc854f26e444d67daedc
Toby Mackenzie <public@tobymackenzie.com>
2021-02-16 00:50:36 -0500

    docs: add comment with mustard cut browser support

--- src/cutsMustard.js
+++ src/cutsMustard.js
@@ -1,1 +1,11 @@
+/*
+https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#browser_compatibility
+
+chrome 42
+edge 14
+firefox 39
+ie -
+opera 29
+safari 10.1
+*/
 export default (window.fetch && window.history) ? true : false;
